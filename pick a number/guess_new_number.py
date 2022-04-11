64cbc4602aa4c3c416350de3fe0064fcd8ae6d63
Sky Brewer <jaco.brewer@gmail.com>
2022-04-11 11:17:10 +0200

    basic if

--- pick a number/guess_new_number.py
+++ pick a number/guess_new_number.py
@@ -3,5 +3,9 @@ import random
 number = random.randint(1, 100)
 
 guess = input("Guess a number, between 1 and 100:")
+guess = int(guess)
 
-print("I'm sorry you got it wrong.")
+if guess != number:
+    print("I'm sorry you got it wrong.")
+else:
+    print("You got it right!")
