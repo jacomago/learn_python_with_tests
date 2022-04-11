99e90f158b7a4635b706e1604e8316b652784286
Sky Brewer <jaco.brewer@gmail.com>
2022-04-11 11:45:28 +0200

    ranges

--- pick a number/guess_new_number.py
+++ pick a number/guess_new_number.py
@@ -1,38 +1,51 @@
 import random
 
-def play_game():
-    number = random.randint(1, 100)
+def get_ranges():
+    guesses = int(input("How many guesses allowed?"))
+    a = int(input("Minimum number?"))
+    b = int(input("Maximum number?"))
+    return a, b, guesses
 
-    for i in range(5):
-        guess_workflow(number)
+def play_game(a, b, guesses):
+    number = random.randint(a, b)
+    correct = False
+    for i in range(guesses):
+        correct = guess_workflow(number, a, b)
+        if correct:
+            return
     
     print("I'm sorry you ran out of guesses.")
 
-def guess_workflow(number):
-    guess = input("Guess a number, between 1 and 100:")
+def guess_workflow(number, a, b):
+    guess = input("Guess a number, between " + str(a) +" and "+ str(b) + ":")
     guess = int(guess)
 
     if guess < number:
         print("Too small!")
+        return False
     elif guess > number:
         print("Too big!")
+        return False
     else:
         print("You got it right!")
+        return True
 
 def play_again():
     while True:
-        play_choice = input("Would you like to play a game? (Y/N)")
+        play_choice = input("Would you like to play again? (Y/N)")
         if play_choice == "N":
             return False
         elif play_choice == "Y":
             return True
         print("Please answer Y or N")
 
-play_game()
+ranges = get_ranges()
+play_game(*ranges)
 
 while True:
     choice = play_again()
     if not choice:
         exit()
     else:
-        play_game()
+        ranges = get_ranges()
+        play_game(*ranges)
