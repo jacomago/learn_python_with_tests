713411df4dd70aafad98beb0484bedf77c05d603
Sky Brewer <jaco.brewer@gmail.com>
2022-04-11 11:18:53 +0200

    Multiple guesses

--- pick a number/guess_new_number.py
+++ pick a number/guess_new_number.py
@@ -2,10 +2,13 @@ import random
 
 number = random.randint(1, 100)
 
-guess = input("Guess a number, between 1 and 100:")
-guess = int(guess)
+for i in range(5):
+    guess = input("Guess a number, between 1 and 100:")
+    guess = int(guess)
 
-if guess != number:
-    print("I'm sorry you got it wrong.")
-else:
-    print("You got it right!")
+    if guess != number:
+        print("I'm sorry you got it wrong, try again.")
+    else:
+        print("You got it right!")
+
+print("I'm sorry you ran out of guesses.")
