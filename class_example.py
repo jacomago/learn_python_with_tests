b4a05b169cb20d786537e8d965fed63ddf2a8659
Sky Brewer <jaco.brewer@gmail.com>
2022-04-19 15:14:11 +0200

    class example

---
+++ class_example.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self, make, model_number, color):
+        self.make = make
+        self.model_number = model_number
+        self.color = color
+    
+    def drive(self, direction):
+        print("drive in direction:", direction)
+
+    def honk_horn(self):
+        print("bwagh")
+
+    def details(self):
+        print("details are ", self.make, self.model_number, self.color)
+
+new_car = Car("ferrari", 34, "red")
+
+new_car.drive("forward")
+new_car.honk_horn()
+new_car.details()
