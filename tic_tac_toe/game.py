185959335407ed14331f16935155417b2b1c1e68
Sky Brewer <jaco.brewer@gmail.com>
2022-04-19 17:06:32 +0200

    refactor

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,82 +1,116 @@
 class Position:
-    def get(self):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get():
         pos = input("Which position, x, y?")
         pos = pos.split(",")
-        self.x = int(pos[0]) - 1
-        self.y = int(pos[1]) - 1
+        return Position(int(pos[0]) - 1, int(pos[1]) - 1)
 
+
+def win_message(player):
+    return "Player " + player + " won!"
 
 class Game:
-    def __init__(self):
+    def __init__(
+        self, board_size=3, empty_board_str="_", player_one="o", player_two="x"
+    ):
+        self.empty_board_str = empty_board_str
+        self.board_size = board_size
         self.board = [
-            ["_" for column_index in range(3)] for row_index in range(3)
-        ]  # [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-        self.player = "o"
-        self.player_two = "x"
+            [empty_board_str for column_index in range(board_size)]
+            for row_index in range(board_size)
+        ]
+        self.player_one = player_one
+        self.player_two = player_two
+        self.current_player = player_one
 
     def position_used(self, position):
-        if self.board[position.y][position.x] != "_":
+        if self.board[position.y][position.x] != self.empty_board_str:
             return True
         return False
 
-    def player_play(self, player):
-        position = Position()
-
+    def get_position(self):
         while True:
-            print("Player ", player, " your turn.")
-            position.get()
+            position = Position.get()
             if not self.position_used(position):
                 break
+            else:
+                print("Position used.")
+        return position
 
+    def update_board(self, player, position):        
         self.board[position.y][position.x] = player
 
+    def reset_player(self):
+        if self.current_player == self.player_one:
+            self.current_player = self.player_two
+        else:
+            self.current_player = self.player_one
 
-    def play(self):
-        self.player_play(self.player)
-        print(self)
-        if self.won(self.player):
-            return 
-        self.player_play(self.player_two)
-        print(self)
+    def play(self, player=None, position=None):
+        if position == None:
+            position = self.get_position()
+        if player == None:
+            player = self.current_player
+
+        self.update_board(player, position)
+        self.reset_player()
 
 
     def won(self, player):
-        winning_list = [player, player, player]
+        winning_list = [player for i in range(self.board_size)]
 
         for row in self.board:
             if row == winning_list:
                 return True
 
         # column check
-        for column_index in range(3):
+        for column_index in range(self.board_size):
             if [
-                self.board[row_index][column_index] for row_index in range(3)
+                self.board[row_index][column_index]
+                for row_index in range(self.board_size)
             ] == winning_list:
                 return True
 
         # diagonal check
-        first_diagonal = [self.board[i][i] for i in range(3)]
+        first_diagonal = [self.board[i][i] for i in range(self.board_size)]
         if first_diagonal == winning_list:
             return True
 
-        second_diagonal = [self.board[i][2 - i] for i in range(3)]
+        second_diagonal = [
+            self.board[i][self.board_size - 1 - i] for i in range(self.board_size)
+        ]
         if second_diagonal == winning_list:
             return True
 
         return False
 
-    def __str__(self):
+    def board_state(self):
         return "\n".join("|".join(row) for row in self.board)
+
+    def __str__(self):
+        board_state = self.board_state()
+        if self.finished():
+            if self.won(self.player_one):
+                return board_state + "\n" + win_message(self.player_one)
+            elif self.won(self.player_two):
+                return board_state + "\n" + win_message(self.player_two)
+            else:
+                return board_state + "\n" + "No winners!"
+        return board_state
+            
 
     def complete_board(self):
         for row in self.board:
             for col in row:
-                if col == "_":
+                if col == self.empty_board_str:
                     return False
         return True
 
     def finished(self):
-        if self.won(self.player) or self.won(self.player_two) or self.complete_board():
+        if self.won(self.player_one) or self.won(self.player_two) or self.complete_board():
             return True
         return False
 
@@ -85,6 +119,7 @@ def main():
     game = Game()
     while not game.finished():
         game.play()
+        print(game)
 
 
 main()
