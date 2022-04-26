4b9523c9af5e17b7648c32e23f19a636861d09e3
Sky Brewer <jaco.brewer@gmail.com>
2022-04-26 15:20:40 +0200

    test variables

--- examples/test_variables.py
+++ examples/test_variables.py
@@ -1,3 +1,19 @@
 
-def test_stuff():
-    assert 1 == 1
+def test_assign():
+    x = 5
+    assert x == 5
+
+def test_change():
+    x = 5
+    x = 6
+    assert x == 6
+
+def test_type():
+    x = 5
+    assert type(x) is int
+
+def test_mult_assign():
+    x = 5
+    y = x
+    assert y == x
+    assert y == 5
