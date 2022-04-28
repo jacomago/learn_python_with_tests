ec2d51e4e587f5b7f0baec9300a54adb001e19e4
Sky Brewer <jaco.brewer@gmail.com>
2022-04-28 14:18:53 +0200

    test game failing formatting

--- tic_tac_toe/test_game.py
+++ tic_tac_toe/test_game.py
@@ -2,17 +2,21 @@ from game import win_message
 from game import Game
 from game import Position
 
+
 def test_win_message():
     assert win_message("o") == "Player o won!"
+
 
 def test_create_game():
     g = Game()
     assert g.empty_board_str == "_"
 
+
 def test_update_board():
     g = Game()
-    g.update_board(g.player_one, Position(1,2))
+    g.update_board(g.player_one, Position(1, 2))
     assert g.board[2][1] == g.player_one
+
 
 def test_game_start():
     g = Game()
