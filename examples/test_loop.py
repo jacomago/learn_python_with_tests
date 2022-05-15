4a28805cf494bdb890bfdb6d483bcbaa2b48bcb6
Sky Brewer <jaco.brewer@gmail.com>
2022-05-15 17:41:23 +0200

    basic loops testing

---
+++ examples/test_loop.py
@@ -0,0 +1,14 @@
+def test_while():
+    sum = 0
+    count = 10
+    while count > 0:
+        sum += 5
+        count -= 5
+    assert count == 0
+    assert sum == 10
+
+def test_for():
+    sum = 0
+    for i in range(0, 10, 5):
+        sum += i
+    assert sum == 5
